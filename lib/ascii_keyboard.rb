43b8da505a54c535d937a367a93f2734a130912a
robmckinnon <rob@movingflow>
2020-02-23 12:03:55 +0000

    Refactor ascii_keyboard to extract const etc

--- lib/ascii_keyboard.rb
+++ lib/ascii_keyboard.rb
@@ -1,26 +1,39 @@
+def s k, v
+  set k, v unless get[k]
+end
+
+s :naturals, %i[C D E F G A B]
+s :sharps, %i[Cs Ds Fs Gs As]
+s :flats, %i[Db Eb Gb Ab Bb]
+s :b_space, %i[_____ _ _____ _ _]
+s :l_space, %i[--|-- - --|-- - -]
+s :space_1, '___'
+s :press_1, '|*|'
+s :nopress, '|_|'
+s :space_2, '  '
+s :press_2, ' *'
+
+def truncate_keys(interval, count)
+   if interval > 10 && count > 3 # above As
+     5
+   elsif interval > 8 # above Gs
+     4
+   elsif interval > 4 # above F
+     3
+   else
+     0
+   end
+end
+
 define :ascii_keyboard do |c, tonic=nil|
   return [] if !c || c.size == 0
-  NATURALS =  %i[C D E F G A B]
-  SHARPS = %i[Cs Ds Fs Gs As]
-  FLATS =  %i[Db Eb Gb Ab Bb]
-  BSPACING = %i[_____ _ _____ _ _]
-  LSPACING = %i[--|-- - --|-- - -]
-  SPACE, PRESS, NONPRESS, SPACE2, PRESS2 = '___', '|*|', '|_|', '  ', ' *'
   ch = c.map{|x| note_info(x).midi_string}.to_a
   first_note = note_info(c[0])
   tonic = tonic && note_info(tonic)
   octave = (tonic || first_note).octave
   width = ((c.last - note("C#{octave}"))/ 11.0 * 7).ceil
 
-  truncate_keys = if first_note.interval > 10 && c.size > 3 # above As
-               5
-             elsif first_note.interval > 8 # above Gs
-               4
-             elsif first_note.interval > 4 # above F
-               3
-             else
-               0
-             end
+  truncate_keys = truncate_keys(first_note.interval, c.size)
   key_count = [7, width].max
   key_count = key_count + truncate_keys if key_count < 10
   scale_count = (key_count / 7) + 1
@@ -40,23 +53,23 @@ define :ascii_keyboard do |c, tonic=nil|
     .join('')[0, charwidth]
   }
 
-  labels, keypresses   = note_labels[NATURALS, scale_count, octave, ch, SPACE2, PRESS2, SPACE2]
-  opts = [scale_count, octave, ch, SPACE, PRESS, NONPRESS]
-  slabels, skeypresses = note_labels[SHARPS, *opts]
-  flabels, fkeypresses = note_labels[FLATS, *opts]
-  blabels     = slabels.zip(flabels).map{|s,f| s == SPACE ? f : s}
-  bkeypresses = skeypresses.zip(fkeypresses).map{|s,f| s == NONPRESS ? f : s}
+  labels, keypresses   = note_labels[get[:naturals], scale_count, octave, ch, get[:space_2], get[:press_2], get[:space_2]]
+  opts = [scale_count, octave, ch, get[:space_1], get[:press_1], get[:nopress]]
+  slabels, skeypresses = note_labels[get[:sharps], *opts]
+  flabels, fkeypresses = note_labels[get[:flats], *opts]
+  blabels     = slabels.zip(flabels).map{|s,f| s == get[:space_1] ? f : s}
+  bkeypresses = skeypresses.zip(fkeypresses).map{|s,f| s == get[:nopress] ? f : s}
 
   charwidth = 1 + key_count*4
 
-  blabels = add_spacing[BSPACING, scale_count, blabels, charwidth]
+  blabels = add_spacing[get[:b_space], scale_count, blabels, charwidth]
   blabels = blabels[2, blabels.size] + '__'
 
-  bkeypresses = add_spacing[LSPACING, scale_count, bkeypresses, charwidth].tr('-',' ')
+  bkeypresses = add_spacing[get[:l_space], scale_count, bkeypresses, charwidth].tr('-',' ')
   bkeypresses = bkeypresses[2, blabels.size] + (mod_key_count_zero ? ' |' : '| ')
   bkeys = bkeypresses.tr('*_',' ')
 
-  labels = (' ' + labels.join(SPACE2))[0, charwidth]
+  labels = (' ' + labels.join(get[:space_2]))[0, charwidth]
   keypresses = ('|' + keypresses.join(' |'))[0, charwidth]
   keys = keypresses.tr(' *','_')
 
